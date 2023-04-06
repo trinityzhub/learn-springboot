00767359959c1de6f203a95d90262e475a176f47
Alpha <alphahuman.one@gmail.com>
2023-04-06 13:24:42 +0000

    what-springboot

---
+++ whatboot_try3/what-springboot/src/main/java/learn/springboot/whatspringboot/WhatSpringbootApplication.java
@@ -0,0 +1,13 @@
+package learn.springboot.whatspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatSpringbootApplication.class, args);
+	}
+
+}
---
+++ whatboot_try3/what-springboot/src/test/java/learn/springboot/whatspringboot/WhatSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.springboot.whatspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
