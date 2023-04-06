1480304eb989857f5bea29ad1f641d37aadcee3b
Alpha <alphahuman.one@gmail.com>
2023-04-06 16:06:54 +0000

    testing list as json

--- whatboot_try3/what-springboot/src/main/java/learn/springboot/whatspringboot/v11/controller/FunRestController.java
+++ whatboot_try3/what-springboot/src/main/java/learn/springboot/whatspringboot/v11/controller/FunRestController.java
@@ -2,8 +2,10 @@ package learn.springboot.whatspringboot.v11.controller;
 
 import java.util.*;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
 
 @RestController
 public class FunRestController {
@@ -13,7 +15,8 @@ public class FunRestController {
     public String sayHello() {
         return "Hi Hello";
     }
-    @RequestMapping("/list")
+    @GetMapping(path = "/list",  produces = MediaType.APPLICATION_JSON_VALUE) 
+    // @GetMapping(path = "/list", consumes = "application/json" ) // Content-Type is not supported
     public List getList() {
 
         List temp = new ArrayList<Integer>();
