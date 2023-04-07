232d051716c3a2271b3e6aa0b2eeb206e4c117e9
Alpha <alphahuman.one@gmail.com>
2023-04-07 14:33:08 +0000

    inject prop values into java

--- whatboot_try3/what-springboot/src/main/java/learn/springboot/whatspringboot/v11/controller/FunRestController.java
+++ whatboot_try3/what-springboot/src/main/java/learn/springboot/whatspringboot/v11/controller/FunRestController.java
@@ -5,15 +5,22 @@ import java.util.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 
 @RestController
 public class FunRestController {
     
+    @Value("${info.app.name}")
+    private String appName;
+
+    @Value("${info.app.version}")
+    private String appVersion;
+
     //
     @RequestMapping("/hello")
     public String sayHello() {
-        return "Hi Hello";
+        return "Welcome "+appName+" version:"+appVersion;
     }
     @GetMapping(path = "/list",  produces = MediaType.APPLICATION_JSON_VALUE) 
     // @GetMapping(path = "/list", consumes = "application/json" ) // Content-Type is not supported
@@ -24,4 +31,5 @@ public class FunRestController {
         temp.add(2);
         return temp;
     }
+    
 }
