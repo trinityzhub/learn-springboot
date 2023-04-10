711c1b654f29e029873a73129b7f113f4d34f331
developer <idea@d45.dev>
2023-04-10 17:59:38 +0000

    Method Injectioon

---
+++ whatboot_try3/what-springboot/src/main/java/learn/springboot/whatspringboot/V12/serviceImpl/BaseballCoach.java
@@ -0,0 +1,15 @@
+package learn.springboot.whatspringboot.V12.serviceImpl;
+
+import learn.springboot.whatspringboot.V12.service.Coach;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BaseballCoach implements Coach {
+
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice 2hr Daily in BALL";
+    }
+
+}
