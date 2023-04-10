6a4c7a4390ce6829973c20ca06b838ca5126be10
developer <idea@d45.dev>
2023-04-10 18:03:44 +0000

    Use Qualifier

--- whatboot_try3/what-springboot/src/main/java/learn/springboot/whatspringboot/V12/web/SportsController.java
+++ whatboot_try3/what-springboot/src/main/java/learn/springboot/whatspringboot/V12/web/SportsController.java
@@ -2,6 +2,7 @@ package learn.springboot.whatspringboot.V12.web;
 
 import learn.springboot.whatspringboot.V12.service.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,7 +26,7 @@ public class SportsController {
 
 */
     @Autowired
-    public void methodInjection(Coach myCoach) {
+    public void methodInjection(@Qualifier("baseballCoach") Coach myCoach) {
         System.out.println("setMyCoach-------------------");
         this.myCoach = myCoach;
     }
