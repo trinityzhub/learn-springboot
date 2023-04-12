fa20396086cc83c2c50e6e75cddddb001b357d66
developer <idea@d45.dev>
2023-04-12 11:45:59 +0000

    v13

--- whatboot_try3/what-springboot/src/main/java/learn/springboot/whatspringboot/WhatSpringbootApplication.java
+++ whatboot_try3/what-springboot/src/main/java/learn/springboot/whatspringboot/WhatSpringbootApplication.java
@@ -3,7 +3,7 @@ package learn.springboot.whatspringboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = { "learn.springboot.whatspringboot.V12", "learn.springboot.whatspringboot.util"} )
+@SpringBootApplication(scanBasePackages = { "learn.springboot.whatspringboot.v13", "learn.springboot.whatspringboot.util"} )
 public class WhatSpringbootApplication {
 
 	public static void main(String[] args) {
