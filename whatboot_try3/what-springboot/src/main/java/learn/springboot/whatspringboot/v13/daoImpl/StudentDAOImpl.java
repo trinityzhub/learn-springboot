1f7aad00b6bf600c9797d5a736da8dd484692dd3
developer <idea@d45.dev>
2023-04-14 10:06:18 +0000

    save Impl

--- whatboot_try3/what-springboot/src/main/java/learn/springboot/whatspringboot/v13/daoImpl/StudentDAOImpl.java
+++ whatboot_try3/what-springboot/src/main/java/learn/springboot/whatspringboot/v13/daoImpl/StudentDAOImpl.java
@@ -1,11 +1,11 @@
 package learn.springboot.whatspringboot.v13.daoImpl;
 
 import jakarta.persistence.EntityManager;
-import jakarta.transaction.Transactional;
 import learn.springboot.whatspringboot.v13.dao.StudentDAO;
 import learn.springboot.whatspringboot.v13.entity.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Repository
