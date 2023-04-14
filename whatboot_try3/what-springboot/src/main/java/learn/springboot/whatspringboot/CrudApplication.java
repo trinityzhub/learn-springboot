077c879f082690365fc6a1d932976d18b995f32f
developer <idea@d45.dev>
2023-04-14 10:06:32 +0000

    dummy data insert

--- whatboot_try3/what-springboot/src/main/java/learn/springboot/whatspringboot/CrudApplication.java
+++ whatboot_try3/what-springboot/src/main/java/learn/springboot/whatspringboot/CrudApplication.java
@@ -1,5 +1,8 @@
 package learn.springboot.whatspringboot;
 
+import learn.springboot.whatspringboot.v13.dao.StudentDAO;
+import learn.springboot.whatspringboot.v13.entity.Student;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,10 +15,29 @@ public class CrudApplication {
 		SpringApplication.run(CrudApplication.class, args);
 	}
 
+	//@Autowired
 	@Bean
-	public CommandLineRunner CommandLineRunner(String[] args) {
+	public CommandLineRunner CommandLineRunner(StudentDAO theStudentDAO)  {  //(String[] args) {
 		return  runner -> {
 			System.out.println("---------------Hello World ; CommandLineRunner ");
+			// createDummyStudents(theStudentDAO);
 		};
 	}
+
+
+	private void createDummyStudents(StudentDAO dao) {
+
+		Student temp1 = new Student("aby", "Koe", "aby@gmail.com");
+		Student temp2 = new Student("Kushi", "Koe", "Kushi@gmail.com");
+		Student temp3 = new Student("Babu", "Drona", "babu@gmail.com");
+		Student temp4 = new Student("Kuyil", "Drona", "aby@gmail.com");
+
+		dao.save(temp1);
+		dao.save(temp2);
+		dao.save(temp3);
+		dao.save(temp4);
+		
+	}
+	
+
 }
