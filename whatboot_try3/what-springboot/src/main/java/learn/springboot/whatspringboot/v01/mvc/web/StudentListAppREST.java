890f1e7908015ca4aa6a30159e565bf24605c67d
ubbe <ubbe@kattegat.e45>
2023-08-08 02:54:52 +0530

    not working

--- whatboot_try3/what-springboot/src/main/java/learn/springboot/whatspringboot/v01/mvc/web/StudentListAppREST.java
+++ whatboot_try3/what-springboot/src/main/java/learn/springboot/whatspringboot/v01/mvc/web/StudentListAppREST.java
@@ -1,24 +1,30 @@
 package learn.springboot.whatspringboot.v01.mvc.web;
 
+
 import com.sun.net.httpserver.HttpServer;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpExchange;
-import learn.springboot.whatspringboot.v01.mvc.entity.Student;
-import learn.springboot.whatspringboot.v01.mvc.service.StudentController;
+
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.util.List;
 
+import learn.springboot.whatspringboot.v01.mvc.service.StudentController;
+import learn.springboot.whatspringboot.v01.mvc.entity.Student;
 
-// StudentListAppV3   REST
+
+
 public class StudentListAppREST {
     public static void main(String[] args) throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(7171), 0);
         server.createContext("/students", new StudentsHandler());
         server.setExecutor(null); // creates a default executor
         server.start();
+
+        System.out.println("RESTful endpoints are running:");
+        System.out.println("GET http://172.18.0.17:7171/students");
     }
 
     static class StudentsHandler implements HttpHandler {
@@ -38,3 +44,6 @@ public class StudentListAppREST {
         }
     }
 }
+
+
+
